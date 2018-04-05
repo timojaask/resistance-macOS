1888c85e052588376344cc7424f58d33dc0b0153
Timo Jaaskelainen <timojaask@gmail.com>
2018-04-05 14:50:04 -0300

    Initial version

---
+++ ResistanceReminder/AppDelegate.swift
@@ -0,0 +1,50 @@
+import Cocoa
+
+let flashInterval = TimeInterval(60 * 5)
+let regularText = "Resistance"
+let attentionText = "RSSTNC"
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var timer: Timer?
+    
+    func drawAttention() {
+        DispatchQueue.main.async {
+            self.setTitle(attentionText, foregroundColor: NSColor.white, backgroundColor: NSColor.red)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.10) {
+            self.setTitle(regularText, foregroundColor: NSColor.white, backgroundColor: NSColor.red)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.20) {
+            self.setTitle(attentionText, foregroundColor: NSColor.white, backgroundColor: NSColor.red)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.30) {
+            self.setTitle(regularText)
+        }
+    }
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    func setTitle(_ title: String, foregroundColor: NSColor, backgroundColor: NSColor) {
+        let attributes = [
+            NSAttributedStringKey.foregroundColor: foregroundColor,
+            NSAttributedStringKey.backgroundColor: backgroundColor
+        ]
+        let attributedText = NSAttributedString(string: title, attributes: attributes)
+        statusItem.length = CGFloat(title.count * 8)
+        statusItem.attributedTitle = attributedText
+    }
+    
+    func setTitle(_ title: String) {
+        statusItem.length = CGFloat(title.count * 8)
+        statusItem.title = title
+    }
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        setTitle(regularText)
+        timer = Timer.scheduledTimer(withTimeInterval: flashInterval, repeats: true, block: { _ in
+            self.drawAttention()
+        })
+    }
+}
+
---
+++ ResistanceReminder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ResistanceReminder
+//
+//  Created by Timo Jääskeläinen on 05/04/2018.
+//  Copyright © 2018 Timo Jääskeläinen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
